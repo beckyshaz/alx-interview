86c10c251d04800061ebbc597d02e15f272a0128
beckyshaz <sharonsakah8@gmail.com>
2024-10-09 10:09:08 +0000

    pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,17 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """implementing pascal triangle using python algorithm"""
 
 
 def pascal_triangle(n):
+    """pascal triangle using loops in python"""
     if (n <= 0):
         return []
-    triangle = [];
+    triangle = []
     for index in range(n):
-        rows = [];
+        rows = []
         for i in range(index + 1):
             if (i == 0) or (i == index):
-                rows.append(1);
+                rows.append(1)
             else:
-                rows.append(triangle[index - 1][i] + triangle[index - 1][i - 1]);
+                rows.append(
+                    triangle[index - 1][i] + triangle[index - 1][i - 1]
+                    )
         triangle.append(rows)
     return triangle
